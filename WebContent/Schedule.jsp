1a465af13e79659ef75817c45e5934c04eb0c153
Laza993 <lazarevicclaza@yahoo.com>
2020-08-23 19:16:14 +0200

    jsp start on branch jsp

---
+++ WebContent/Schedule.jsp
@@ -0,0 +1,42 @@
+<%@page import="Schedule.model.User"%>
+<%@page import="Schedule.model.Role"%>
+
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	User loggedUser = (User) session.getAttribute("loggedUser");
+%>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schedule</title>
+    <style>
+        a:link, a:visited {
+            background-color: #4085f5;
+            color: white;
+            padding: 10px 15px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            border-radius: 10px;
+        }
+
+        a:hover, a:active {
+            background-color: blue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to School Schedule</h1>
+<% if (loggedUser == null) { %>
+    <a href="Login.html">Please sign in</a>
+<% } else if (loggedUser.getRole().equals(Role.student)){ %>
+    <a href="AllLecturesServlet">Preview schedule</a>
+<% } else { %>
+    <a href="AllLecturesServlet">Preview schedule</a>
+    <a href="ViewAddLectureServlet">Add a new Lecture</a>
+<% } %>
+</body>
+</html>
--- src/Schedule/ScheduleServlet.java
+++ src/Schedule/ScheduleServlet.java
@@ -1,76 +1,19 @@
 package Schedule;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import Schedule.model.Role;
-import Schedule.model.User;
 
 
 @SuppressWarnings("serial")
 public class ScheduleServlet extends HttpServlet {
        
-    public ScheduleServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		User loggedUser = (User) session.getAttribute("loggedUser");
-		
-		response.setContentType("text/html; charset=UTF-8");
-		
-		PrintWriter pw = response.getWriter();
-		pw.write("<!DOCTYPE html>\r\n" + 
-				"<html lang=\"en\">\r\n" + 
-				"<head>\r\n" + 
-				"    <meta charset=\"UTF-8\">\r\n" + 
-				"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + 
-				"    <title>Schedule</title>\r\n" + 
-				"    <style>\r\n" + 
-				"        a:link, a:visited {\r\n" + 
-				"            background-color: #4085f5;\r\n" + 
-				"            color: white;\r\n" + 
-				"            padding: 10px 15px;\r\n" + 
-				"            text-align: center;\r\n" + 
-				"            text-decoration: none;\r\n" + 
-				"            display: inline-block;\r\n" + 
-				"            border-radius: 10px;\r\n" + 
-				"        }\r\n" + 
-				"\r\n" + 
-				"        a:hover, a:active {\r\n" + 
-				"            background-color: blue;\r\n" + 
-				"        }\r\n" + 
-				"    </style>" +
-				"</head>\r\n" + 
-				"<body>\r\n" + 
-				"    <h1>Welcome to School Schedule</h1>\r\n");
-				if(loggedUser == null) {
-					pw.write("<a href=\"Login.html\">Please Sing in</a>\r\n");
-				}else if(loggedUser.getRole().equals(Role.student)) {
-					pw.write("<a href=\"AllLecturesServlet\">Preview schedule</a>\r\n");
-				}
-				else {
-					pw.write("<a href=\"AllLecturesServlet\">Preview schedule</a>\r\n" + 
-				"    <a href=\"ViewAddLectureServlet\">Add a new Lecture</a>\r\n");
-				}
-				pw.write(
-						"</body>\r\n" + 
-						"</html>"
-						);
-
+		request.getRequestDispatcher("Schedule.jsp").forward(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
